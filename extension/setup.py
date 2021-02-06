26026b5ec07a5771540eae9c10b3689058258e2d
Alex Dillhoff <ajdillhoff@gmail.com>
2021-02-06 15:08:59 -0600

    update: Specifying c++14 in build options.

--- extension/setup.py
+++ extension/setup.py
@@ -6,9 +6,11 @@ setup(name='compute_mesh_mask',
           CUDAExtension('compute_mesh_mask_cuda', [
               'gpu/compute_mesh_mask_cuda.cpp',
               'gpu/compute_mesh_mask.cu'
-          ]),
+          ],
+                        extra_compile_args=['-std=c++14']),
           CppExtension('compute_mesh_mask', [
               'cpp/compute_mesh_mask.cpp'
-          ])
+          ],
+                       extra_compile_args=['-std=c++14'])
       ],
       cmdclass={'build_ext': BuildExtension})
